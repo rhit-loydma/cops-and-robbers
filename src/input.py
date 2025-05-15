116a0c28d171c8504d11b894826ba1b62c19a8a5
Bender <benderet@rose-hulman.edu>
2025-05-14 23:09:09 -0400

    changed commands to take input arguments instead of asking for them after, also implemented saving and loading graphs from file

--- src/input.py
+++ src/input.py
@@ -1,4 +1,5 @@
 import networkx as nx
+import pickle
 from networkx.classes.graph import Graph
 from viz import draw_graph
 from algorithm import set_attributes
@@ -22,34 +23,42 @@ def addNextNode(graph: Graph) -> int:
 
 def commandLoop(graph: Graph):
     while True:
-        print("q - quit, n - add node, e - add edge, r - remove edge, d - draw graph")
-        command = input()
-        if command == 'q':
+        print("q - quit, n - add node, e NODE1 NODE2 - add edge, r NODE1 NODE2 - remove edge, d - draw graph, s NAME - save graph, l NAME - load graph")
+        commands = input().split(" ")
+        command = commands[0]
+        if command == 'q' and len(commands) == 1:
             exit()
-        if command == 'n':
+        if command == 'n' and len(commands) == 1:
             index: int = addNextNode(graph)
             print("Added node index " + str(index))
-        if command == 'e':
-            print("Input index of first node")
-            node = int(input())
-            print("Input index of second node")
-            node2 = int(input())
+        if command == 'e' and len(commands) == 3:
+            node = int(commands[1])
+            node2 = int(commands[2])
             success: bool = addEdge(graph, node, node2)
             if success: 
                 print("Added edge from " + str(node) + " to " + str(node2))
             else:
                 print("Edge from " + str(node) + " to " + str(node2) + " already exists")
-        if command == 'r':
-            print("Input index of first node")
-            node = int(input())
-            print("Input index of second node")
-            node2 = int(input())
+        if command == 'r' and len(commands) == 3:
+            node = int(commands[1])
+            node2 = int(commands[2])
             success: bool = removeEdge(graph, node, node2)
             if success: 
                 print("Removed edge from " + str(node) + " to " + str(node2))
             else:
                 print("Edge from " + str(node) + " to " + str(node2) + " doesn't exist")
-        if command == "d":
+        if command == "d" and len(commands) == 1:
+            if graph.number_of_edges() == 0:
+                print("You need to add an edge before drawing the graph.")
+                continue
             print("Drawing graph. Close window to continue editing the graph.")
             set_attributes(graph)
             draw_graph(graph)
+        if command == "s" and len(commands) == 2:
+            file = commands[1]
+            with open(file, "wb") as f:
+                pickle.dump(graph, f)
+        if command == "l" and len(commands) == 2:
+            file = commands[1]
+            with open(file, 'rb') as file:
+                graph = pickle.load(file)
